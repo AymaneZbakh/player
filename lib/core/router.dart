ae377557be9944f98b2f3f9c440a6e01fbf157d0
AymaneZbakh <75531985+AymaneZbakh@users.noreply.github.com>
2026-06-09 20:02:19 +0100

    Update router.dart

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../data/models/playlist.dart';
-import '../data/models/channel.dart';
 import '../ui/screens/home_screen.dart';
 import '../ui/screens/channel_dashboard_screen.dart';
-import '../ui/screens/player_screen.dart';
-import '../ui/screens/settings_screen.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
@@ -18,11 +14,6 @@ final appRouterProvider = Provider<GoRouter>((ref) {
         path: '/channels',
         builder: (context, state) => ChannelDashboardScreen(playlist: state.extra as Playlist),
       ),
-      GoRoute(
-        path: '/player',
-        builder: (context, state) => PlayerScreen(channel: state.extra as Channel),
-      ),
-      GoRoute(path: '/settings', builder: (context, state) => const SettingsScreen()),
     ],
   );
 });
